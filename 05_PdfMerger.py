76dff513597cd288cdf58ea7d88516e13d38ad87
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2023-03-26 09:29:56 +0530

    Creating PDF Merger

--- 05_PdfMerger.py
+++ 05_PdfMerger.py
@@ -1,14 +1,14 @@
 import PyPDF2 as pdf
 
-# path1 = "05_PdfMerger/1.pdf"
-# path2 = "05_PdfMerger/2.pdf"
+# path1 = "sample.pdf"
+# path2 = "dummy.pdf"
 # pdfFilies = [path1, path2]
-n = int(input('Enter number of pdfs: '))
+n = int(input('Enter number of pdf file you want to merge: '))
 
 pdfFilies = []
 for i in range (0, n):
     file = input("Enter pdf file name: ")
-    pdfFilies.append(f"05_PdfMerger/{file}.pdf")
+    pdfFilies.append(f"{file}.pdf")
 print(pdfFilies)
 
 merger = pdf.PdfMerger()
@@ -17,4 +17,4 @@ for filename in pdfFilies:
     pdfReader = pdf.PdfReader(pdfFilies)
     merger.append(pdfReader)
 pdfFilies.close()
-merger.write('05_PdfMerger/merged.pdf')
+merger.write('merged.pdf')
